1b44efdb707b7d3472ee4b1d21cbef8dbd0ae1cd
Richard <richgibson123@hotmail.co.uk>
2024-12-14 19:17:13 +0000

    day-card weather icon lookup display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { TextCarouselComponent } from './components/text-carousel/text-carousel.component';
 import { Forecast, ForecastState, WeatherDataService } from './services/weather-data.service';
 import { Subscription } from 'rxjs';
+import { WmoIconMapping } from './services/sys-data.service';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,14 @@ import { Subscription } from 'rxjs';
   providers: [WeatherDataService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'WeatherEnvitia';
   dataSub: Subscription;
   forecast: Forecast;
   forecastDays: Array<string> = [];
+  wmoMappings: WmoIconMapping;
 
   constructor(
     protected data: WeatherDataService, 
@@ -28,13 +30,18 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    this.wmoMappings = this.data.WmoMappings;
+
     this.data.getForecast();
     this.dataSub = this.data.forecast$.subscribe(
       (state: ForecastState) => {
         this.forecast = state.forecast;
         this.forecastDays = [...Object.keys(this.forecast)];
-        this.cd.detectChanges();
         console.log(this.forecastDays);
+            
+        console.log(this.wmoMappings[0].day.description)
+        // this.cd.detectChanges();
+
       } 
     )
   }
